2124211c51cd08317413da598607075bc9b475b8
Andrew DuBrock <adubrock@comcast.net>
2018-04-18 23:49:51 -0700

    basic nginx

--- manifests/nginx-config.pp
+++ manifests/nginx-config.pp
@@ -3,15 +3,15 @@
 # install and bootstrap an nginx instance
 
 class {'nginx':
-  manage_repo => true,
-  package_source => 'nginx-stable'
+#  manage_repo => true,
+#  package_source => 'nginx-stable'
   }
 
 #download the file from github (https://github.com/puppetlabs/exercise-webpage)
 # Need to do. Covered in Puppetfile
 
 # serve up the file
-nginx::resource::server { '10.0.2.15':
+  nginx::resource::server { '10.0.2.15':
   listen_port => 80,
   www_root => '/www/homepage/',
-}
+ }
